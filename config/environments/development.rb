fc2e2d6e49238d6b0d636bd341d7a3f2e85383d2
EC2 Default User <ec2-user@ip-172-31-47-221.ap-southeast-1.compute.internal>
2019-02-13 14:10:00 +0000

    web_console追加

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,4 +60,6 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   
   config.web_console.development_only = false
+  config.assets.compile = true
+  
 end
